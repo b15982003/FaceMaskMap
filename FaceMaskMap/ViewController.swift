2803f2219fa9880ff9d19c6699b4af80460afa92
張睿平 <changrueiping@zhangruipingdeMacBook-Air.local>
2024-07-20 15:22:29 +0800

    Create project

---
+++ FaceMaskMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceMaskMap
+//
+//  Created by 張睿平 on 2024/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
